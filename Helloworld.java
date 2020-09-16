6b25bf08dd6d41266135fc6ed472bdd4fdb304ff
Aotoge <kadambari.anuranjana@gmail.com>
2020-09-16 09:01:59 +0000

    add crazy experiment

--- Helloworld.java
+++ Helloworld.java
@@ -2,7 +2,7 @@ class Helloworld{
 	public static void main(String args[]){
 		int i=0;
 		for(i=0;i<5;i++){
-			System.out.println("Hello Citi!");
+			System.out.println("Hello Citi! In experimental branch!!");
 		}
 	}
 }
